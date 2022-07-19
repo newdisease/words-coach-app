25f239ed3b5bbb00dc20c05b5dc0c981615e6f3c
Andrii Kryvulko <newdisease91@gmail.com>
2022-07-19 09:41:34 +0200

    setup dir structure

---
+++ backend/config/settings/local 2.py
@@ -0,0 +1,57 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = ["*"]
+
+# Apps that will work only in development mode
+INSTALLED_APPS += []
+
+# Middlewares that will work only in development mode
+MIDDLEWARE += []
+
+# Setting for django-cors-headers library
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+# Parse database connection url strings
+# like psql://user:pass@127.0.0.1:8458/db
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME', default='postgres'),
+        'USER': env('DATABASE_USER', default='postgres'),
+        'PASSWORD': env('DATABASE_PASSWORD', default='postgres'),
+        'HOST': env('DATABASE_HOST', default='db'),
+        'PORT': '5432',
+    }
+}
+
+# Monitor database queries and log them to the console
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    },
+}
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./components/app/App";
+import App from "./Components/App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
