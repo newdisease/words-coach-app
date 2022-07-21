8999ca2ae610108a4f5089117eed3781634f306b
Andrii Kryvulko <newdisease91@gmail.com>
2022-07-21 19:44:29 +0200

    add react router

---
+++ frontend/src/Components/Pages/index.jsx
@@ -0,0 +1,6 @@
+import DictionaryPage from "./DictionaryPage";
+import QuizPage from "./QuizPage";
+import ErrorPage from "./ErrorPage";
+import MainPage from "./MainPage";
+
+export { DictionaryPage, QuizPage, ErrorPage, MainPage };
