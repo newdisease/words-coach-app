b3d0e9633e011ec8ee006120d4d2b45170432608
Andrii Kryvulko <newdisease91@gmail.com>
2022-07-21 16:20:02 +0200

    add 404 page

---
+++ frontend/src/Components/Pages/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+const ErrorPage = () => {
+    return (
+        <>
+            <Row style={{ "min-height": "50vh" }} className="d-flex justify-content-center align-items-center">
+                <Col xs={10} md={9} lg={6} className="text-center">
+                    <span className="display-1 d-block">404</span>
+                    <div className="mb-4 lead">The page you are looking for was not found.</div>
+                    <Button className='col-sm-5' type="submit" variant="primary" size="lg" href="/#" >Back to the main page</Button>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default ErrorPage;
