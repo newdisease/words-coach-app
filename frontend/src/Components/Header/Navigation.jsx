03eb5f263ad4cafffd8ba85a628b34193119ef6f
Andrii Kryvulko <newdisease91@gmail.com>
2022-07-21 14:26:20 +0200

    add auth and registration modals

--- frontend/src/Components/Header/Navigation.jsx
+++ frontend/src/Components/Header/Navigation.jsx
@@ -1,26 +1,46 @@
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useState } from 'react';
+import ModalAuth from '../ModalUser/ModalAuth';
+import ModalCreateAccount from '../ModalUser/ModalCreateAccount';
 
 const Navigation = () => {
+    const [modalAuthShow, setModalAuthShow] = useState(false);
+    const [modalRegistrationShow, setModalRegistrationShow] = useState(false);
     return (
-        <Navbar bg="light" className='mt-5'>
-            <Navbar.Brand href="#">Word coach app</Navbar.Brand>
-            <Nav>
-                <Nav.Link href="#">
-                    Dictionary
-                </Nav.Link>
-            </Nav>
-            <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-                <Nav className="">
-                    <NavDropdown title="John Dou" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#">
-                            Logout
-                        </NavDropdown.Item>
-                    </NavDropdown>
+        <>
+            <Navbar bg="light" className='mt-5'>
+                <Navbar.Brand href="#">Word coach app</Navbar.Brand>
+                <Nav>
+                    <Nav.Link href="#">
+                        Dictionary
+                    </Nav.Link>
                 </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+                    <Nav className="">
+                        <NavDropdown title="John Dou" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#">
+                                Logout
+                            </NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => setModalAuthShow(true)}>
+                                Login
+                            </NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => setModalRegistrationShow(true)}>
+                                Registration
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <ModalCreateAccount
+                show={modalRegistrationShow}
+                onHide={() => setModalRegistrationShow(false)}
+            />
+
+            <ModalAuth
+                show={modalAuthShow}
+                onHide={() => setModalAuthShow(false)}
+            />
+        </>
     );
 }
 
---
+++ frontend/src/Components/ModalUser/ModalAuth.jsx
@@ -0,0 +1,39 @@
+import { Button, Modal, Form } from 'react-bootstrap';
+
+const ModalAuth = (props) => {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Log in
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button className='col-sm-5 mx-auto' type="submit" variant="primary" size="lg">Login</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default ModalAuth;
