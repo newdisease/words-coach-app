d04d0cf8e15a441f0b4ded86086c6a29bb3173ca
Andrii Kryvulko <newdisease91@gmail.com>
2022-08-08 19:18:46 +0200

    make the endpoint that gives random words for a quiz

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework import routers
-from api.views import DictionaryCreateListViewSet
+from api.views import DictionaryCreateListViewSet, DictionaryRandomViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'dictionary', DictionaryCreateListViewSet, 'dictionary')
+router.register(r'quiz', DictionaryRandomViewSet, 'quiz')
 
 urlpatterns = router.urls
--- backend/api/views.py
+++ backend/api/views.py
@@ -23,3 +23,15 @@ class DictionaryCreateListViewSet(
 
     def get_serializer_context(self):
         return {'user_id': self.request.user.id}
+
+
+class DictionaryRandomViewSet(ListModelMixin, GenericViewSet):
+    serializer_class = DictionarySerializer
+
+    def get_queryset(self):
+        return Dictionary.objects.filter(
+            user_id=self.request.user.id, progress__lt=3
+        ).order_by('?')[:5]
+
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id}
